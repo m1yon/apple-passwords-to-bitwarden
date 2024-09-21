d413302a60449947919825a79a00721f5a18a149
Michael Lyon <mlyon360@gmail.com>
2024-09-21 09:55:21 -0600

    add converter

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import pandas as pd
+
+
 def main():
-    print("hello world")
+    apple_passwords_df = pd.read_csv("Passwords.csv")
+
+    bitwarden_df = apple_passwords_df.rename(
+        columns={
+            "Title": "name",
+            "URL": "login_uri",
+            "Username": "login_username",
+            "Password": "login_password",
+            "Notes": "notes",
+            "OTPAuth": "login_totp",
+        }
+    )
+
+    bitwarden_df.to_csv("bitwarden-passwords.csv", index=False)
 
 
 if __name__ == "__main__":
