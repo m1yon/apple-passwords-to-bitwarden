e5b07ba70c66febcfa8a573f839b70295ee4320a
Michael Lyon <mlyon360@gmail.com>
2024-09-21 09:56:54 -0600

    use flag for input file path

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import sys
+
 import pandas as pd
 
 
-def main():
-    apple_passwords_df = pd.read_csv("Passwords.csv")
-
+def main(input_file: str):
+    apple_passwords_df = pd.read_csv(input_file)
     bitwarden_df = apple_passwords_df.rename(
         columns={
             "Title": "name",
@@ -14,9 +15,13 @@ def main():
             "OTPAuth": "login_totp",
         }
     )
-
     bitwarden_df.to_csv("bitwarden-passwords.csv", index=False)
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <input_file>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    main(input_file)
